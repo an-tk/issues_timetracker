3dca6f8f25d455b231899f8935c2132d96482453
Tkachenko <supadm.fs@gmail.com>
2012-10-31 16:33:09 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+IssuesTimetracker::Application.config.secret_token = '2e2e08b8792acae454c0f1ca5f150561949dfb43ed90c8eab56579b7909f98af7c833d4ba689df0fea375185ff944ef4a0b84b8bf1712e0d461dfa8662b399dc'
